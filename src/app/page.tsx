c6e8bb026ba924a03b8229c2d5c9826217c8b7ad
Bananayosostene <sbananayo98@gmail.com>
2024-11-04 23:05:52 +0200

    adding fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      <p> welcome to our website!</p>
+    <div className='flex  items-center justify-center min-h-screen'>
+    <div className='font-sans border-[1px] w-1/2 p-4 bg-darkBg border-gray-600 rounded-[20px]'>
+        <p className='text-sm'> welcome to our website!</p>
+        <hr className="my-4 border-gray-600 w-full" />
       <Link href="/blog">go to blog</Link>
+    </div>
     </div>
   )
 }

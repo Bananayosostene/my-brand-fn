2fa0448948b94b14875b061b76ce293ca086f883
Bananayosostene <sbananayo98@gmail.com>
2024-11-07 21:36:12 +0200

    fixing deployment errors

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const SectionHeader: React.FC<{}> = () => {
+const SectionHeader: React.FC= () => {
   return (
     <div
       className={`w-[1px] h-6 bg-gray-300 dark:bg-gray-600 `}
